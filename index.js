d596c1af7f2f38576efdcd637a361bbe49a05a89
sahincoder <sahincoderbd@gmail.com>
2025-09-26 06:25:49 +0600

    .env file added

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// default route
+app.get("/", (req, res) => {
+  res.send("Coffee shop server");
+});
+
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@coffeelist.oikmndh.mongodb.net/?retryWrites=true&w=majority&appName=CoffeeList`;
+
+
+// MongoDB setup
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const coffeelistDB = client.db("coffeelistDB");
+    const coffeesCollection = coffeelistDB.collection("coffees");
+
+    // GET all coffees
+    app.get("/coffees", async (req, res) => {
+      const result = await coffeesCollection.find().toArray();
+      res.send(result);
+    });
+
+    // coffee details route
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const cursor = await coffeesCollection.findOne(query);
+      res.send(cursor);
+
+    });
+
+    // Update coffee
+    app.put('/coffee/update/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)};
+      const updatedCoffee=req.body;
+      const doc={
+        $set:{
+          name:updatedCoffee.name,
+          chef:updatedCoffee.chef,
+          supplier:updatedCoffee.supplier,
+          price:updatedCoffee.price,
+        }
+      }
+      const option={upsert:false};
+      const result=await coffeesCollection.updateOne(query,doc,option);
+      res.send(result);
+      console.log('Request for update',id);
+    })
+
+    // Delete Coffee
+
+    app.delete('/coffee/delete/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)};
+      const result = await coffeesCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // POST a new coffee
+    app.post("/coffees", async (req, res) => {
+      const coffee = req.body;
+      const result = await coffeesCollection.insertOne(coffee);
+      res.send(result);
+      console.log("New Coffee Added:", coffee);
+    });
+
+    console.log("✅ Connected to MongoDB!");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("🚀 Coffee Shop Server is running on port:", port);
+});
